f0410ba0e41bb954978d358f0e75301127bc08e8
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2023-10-12 11:13:24 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,15 +1,10 @@
 import { galleryItems } from "./gallery-items.js";
-import basicLightbox from 'basiclightbox';
-
-console.log(galleryItems);
-
-
 
 const galleryList = document.querySelector(".gallery");
 
 function createImagesList() {
   return galleryItems
-    .map(({description, original, preview}) => {
+    .map(({ description, original, preview }) => {
       return `<li class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
@@ -21,20 +16,22 @@ function createImagesList() {
   </a>
 </li>`;
     })
-      .join("");
-    
+    .join("");
 }
 galleryList.insertAdjacentHTML("beforeend", createImagesList());
 
-console.log(createImagesList);
+galleryList.addEventListener("click", (event) => {
+  event.preventDefault();
 
-galleryList.addEventListener("click", (e) => {
-    e.preventDefault();
-    if (e.target.tagName === "IMG") {
-        const originalImageUrl = e.target.dataset.source;
-        const instance = basicLightbox.create(`<img src="${original}" width="800" height="600">`);
-        instance.show();
-    }
-    });
+  if (event.target.tagName === "IMG") {
+    const originalImageUrl = event.target.dataset.source;
+    const instance = basicLightbox.create(
+      `<img src="${originalImageUrl}" width="800" height="600">`
+    );
 
-instance.show()
+    instance.element().innerHTML = `<img src="${originalImageUrl}" width="800" height="600">`;
+    console.log(instance);
+
+    instance.show();
+  }
+});

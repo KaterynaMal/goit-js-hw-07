8bdfaeecaf46179a7be473a12ba7616ede74dcae
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2023-10-12 12:00:51 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+function createImagesList() {
+  return galleryItems
+    .map(({ description, original, preview }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join("");
+}
+
+galleryList.insertAdjacentHTML("beforeend", createImagesList());
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+galleryList.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (event.target.tagName === "IMG") {
+    lightbox.open();
+  }
+});
